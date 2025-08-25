ecc6a0a99330178041e93a5a7f327094e2ced12a
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2025-08-25 09:21:43 -0600

    who are you

--- assignments/Who_are_you.py
+++ assignments/Who_are_you.py
@@ -8,4 +8,4 @@ age = input()
 print("What is your favorite color?")
 fav_color = input()
 
-print("Might I acquirest your name peasant?... Ahh,",name,"is a fine title! Might I know thine age in years?...",age, "You have the skin of an elf! What is thine favorite hue?...",fav_color".","That's a beautiful color! How absolutely marvelous!")
+print("Might I acquirest your name peasant?... Ahh,",name,"is a fine title! Might I know thine age in years?...",age, "You have the skin of an elf! What is thine favorite hue?...",fav_color,"That's a beautiful color! How absolutely marvelous!")
