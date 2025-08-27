fc22abd722cba61b9d3e64a022bccef19b3d87c2
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2025-08-27 09:53:08 -0600

    integers and floats notes

---
+++ Notes/numbers.py
@@ -0,0 +1,41 @@
+# IC 1st integers and floats notes
+
+#integers
+age = 999999999999999999
+year = 2015
+room = 200
+hour = 9
+motivation = -5
+
+#float (floating point number)
+pi = 3.14
+percent = .75
+damage = 9.75
+
+#math
+print(5+2) #7
+print(5-2) #3
+print(5*2) #10
+print(5/2) #2.5
+print(5//2) #2
+print(5%2) #1
+print(5**2) #25
+
+print(5+2 *9 + (5**5)%3)
+
+#Type casting
+num = int(3.14) #Does not round!
+decimal = float(8)
+print(num)
+print(decimal)
+
+#using variables
+hours = 3
+minutes = 4
+
+#to change a user input data type you have to surrround the 
+print(hours*60+minutes)
+
+age = int(input("How old are you: \n"))
+
+print("Wow you are old, that is", 525600*age, "minutes!")
