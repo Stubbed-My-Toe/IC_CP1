03490ef229941e638e4a0b3ff82b8270cfac9483
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2025-09-03 12:16:14 -0600

    Update madlib.py

--- assignments/madlib.py
+++ assignments/madlib.py
@@ -1,6 +1,6 @@
 #IC 1st Madlib
 
-print("Give me a noun")
+noun1print("Give me a noun")
 noun1 = input()
 
 print("Give me an adjective")
@@ -45,4 +45,4 @@ noun14 = input()
 print("Give me a part of the human body")
 body_part15 = input()
 
-madlib ="Ye can always be a bloodthirsty pirate " + noun1 + ", threatening everyone by waving yer " + adjective2 + " sword in the air, but until ye learn to " + verb3 + " like a pirate, ye'll never be " + adverb4 + "  " + noun5 + adjective6 + plural_noun7 + plural_noun8 + plural_noun9 + body_part10 + noun11 + noun12 + noun13 + noun14 + body_part15
+madlib = print("Ye can always be a bloodthirsty pirate " + noun1 + ", threatening everyone by waving yer " + adjective2 + " sword in the air, but until ye learn to " + verb3 + " like a pirate, ye'll never be " + adverb4 + " accepted as an authetic " + noun5 + ", So here's what ye do: Cleverly work into yer daily conversations " +adjective6 + " pirate phrases such as 'Ahoy there, "+ plural_noun7 + ",' 'Avast, ye " + plural_noun8 + ",' and 'Shiver me " + plural_noun9 + ",'  " + body_part10 + noun11 + noun12 + noun13 + noun14 + body_part15)
