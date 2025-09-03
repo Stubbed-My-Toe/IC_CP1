a0cebc8427f2fd3fca791b89cc4d94527dcc8b61
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2025-09-03 09:53:08 -0600

    madlib

---
+++ assignments/madlib.py
@@ -0,0 +1,48 @@
+#IC 1st Madlib
+
+print("Give me a noun")
+noun1 = input()
+
+print("Give me an adjective")
+adjective2 = input()
+
+print ("Give me a verb")
+verb3 = input()
+
+print("Give me an adverb")
+adverb4 = input()
+
+print("Give me a noun")
+noun5 = input()
+
+print("Give me an adjective")
+adjective6 = input()
+
+print("Give me a plural noun")
+plural_noun7 = input()
+
+print("Give me another plural noun")
+plural_noun8 = input()
+
+print("Give me another pluarl noun")
+plural_noun9 = input()
+
+print("Give me a part of the human body")
+body_part10 = input()
+
+print("Give me a noun")
+noun11 = input()
+
+print("Give me another noun")
+noun12 = input()
+
+print("Give me another noun")
+noun13 = input()
+
+print("Give me another noun")
+noun14 = input()
+
+print("Give me a part of the human body")
+body_part15 = input()
+
+madlib ="Ye can always be a bloodthirsty pirate " + noun1 + ", threatening everyone by waving yer " + adjective2 + " sword in the air, but until ye learn to " + verb3 + " like a pirate, ye'll never be " + adverb4 + "  " + noun5 + adjective6 + plural_noun7 + plural_noun8 + plural_noun9 + body_part10 + noun11 + noun12 + noun13 + noun14 + body_part15
