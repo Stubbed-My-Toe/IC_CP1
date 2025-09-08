0cc2c833101ea47398c296c07344ffc91bc01385
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2025-09-08 09:53:38 -0600

    calculator

---
+++ assignments/calculator.py
@@ -0,0 +1,12 @@
+#IC 1st period Calculator
+
+print("What is your first number?")
+number1 = input().strip()
+
+print("What is your second number?")
+number2 = input().strip()
+
+multiplication = number1 * number2
+division = number1 / number2
+addition = number1 + number2
+subtraction = number1 - number2
