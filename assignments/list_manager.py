a3e7c8345ad0258bd542681506e6e1d08e5f8148
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2025-09-30 18:15:25 +0000

    list manager

--- assignments/list_manager.py
+++ assignments/list_manager.py
@@ -3,13 +3,16 @@
 print("What items are on your shopping list? To add an item, enter 'add'. To remove an item, enter 'remove'. To print your list, enter 'print'. To finalize and print your total list, enter 'exit'.\n")
 shop_list = []
 while True:
-    action = input("Would you like to 'add', 'remove', 'print', or 'done'?\n")
+    action = input("Would you like to 'add', 'remove', 'print', or 'exit'?\n").lower().strip()
     if action == "add":
         new_thing = input("What would you like to add?\n").strip().lower()
         shop_list.append(new_thing)
     elif action == "remove":
-        delete = input("What would you like to remove?").strip().lower()
-        shop_list.remove(delete)
+        delete = input("What would you like to remove?\n").strip().lower()
+        if delete in shop_list:
+            shop_list.remove(delete)
+        else:
+            print("Please try again.")
     elif action == "print":
         show = print(shop_list)
     elif action == "exit":
