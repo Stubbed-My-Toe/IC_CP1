5a49ba489976b00014c506a8c79d34892bc94134
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2025-10-10 09:26:18 -0600

    psuedocode random number

---
+++ starters/random_number_gen.py
@@ -0,0 +1,30 @@
+#IC 1st ran_num_gen tic_tac_toe
+
+if computer = [1],[5],[9]:
+    print("Computer Won!")
+else:
+    continue
+
+
+if computer = [7],[5],[3]:
+    print("Computer Won!")
+else:
+    continue
+
+
+if computer = [1],[4],[7]:
+    print("Computer Won!")
+else:
+    continue
+
+
+if computer = [2],[5],[8]:
+    print("Computer Won!")
+else:
+    continue
+
+
+if computer = [3],[6],[9]:
+    print("Computer Won!")
+else:
+    continue
