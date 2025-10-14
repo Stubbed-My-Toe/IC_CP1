9965d908b2ed236c961eacaa5d7adf416171063b
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2025-10-14 09:53:24 -0600

    password strength check

--- assignments/password_strength_checker.py
+++ assignments/password_strength_checker.py
@@ -23,19 +23,45 @@ print("Hello! This is the Isaac Covington 2100 Password Strength Checker System;
 print("This program checks the strength of your password based on certain criteria, and gives you a score out of 5.")
 
 #ask for password, show requirements.
-print("Your password must have a length of 8 characters, contain at least one special character, contain at least one capital letter, one lowercase letter, and one number. Meeting one requirement")
+print("Your password must have a length of 8 characters, contain at least one special character, contain at least one capital letter, one lowercase letter, and one number.\n Meeting one requirement gives you one point.")
 #have user enter their password
-paswrd = input('What is your password?\n').strip
+paswrd = input('What is your password?\n').strip()
 
-spec_char_list = list("!@{#$%^&*()_+-=[]}|;:,.<>?")
-numbers = list("1234567890")
+spec_char = ("!@{#$%^&*()_+-=[]}|;:,.<>?")
+numbers = ("1234567890")
+points = 0
 length = len(paswrd)
 
+if length >= 8:
+    points + 1
+else:
+    print("")
 #turn password into list.
-characters = list(paswrd)
+characters = (paswrd)
 #loop for i in password
 for i in characters:
-    special = bool((letter).find[special_character])
+    special = i in spec_char
+    if special:
+        points + 1
+        print("Your points increased")
+    else:
+        print("This special check did not work")
+    num = i.isdigit()
+    if num:
+        points +1
+    else:
+        print("This num check did not work")
+    upperc = i.isupper()
+    if upperc:
+        points +1
+    else:
+        print('')
+    lowerc = i.islower()
+    if lowerc:
+        points +1
+    else:
+        print("")
+    
 #Check each character isupper if at least one is. +1 point
 #Check each character islower if at least one is. +1 point
 #if len(list) >= 8. +1 point
@@ -43,14 +69,13 @@ for i in characters:
 #check each character if it matches the things on special characters list.
 
 
-#if points <= 2
-    #print weak
-
-#if points = 3
-    #print moderate
-
-#f points = 4
-    #print strong
-
-#if points = 5
-    #print very strong
+if points <= 2:
+   print("weak")
+elif points == 3:
+   print("moderate")
+elif points == 4:
+    print ("strong")
+elif points == 5:
+    print("very strong")
+else:
+    print ('no points at all? \n Unfathomably weak')
