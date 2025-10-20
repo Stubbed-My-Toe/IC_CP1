d7c87b9b05385bd5b851289c8e0d1b4ba4b78a77
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2025-10-20 09:53:15 -0600

    functions

---
+++ Notes/functions.py
@@ -0,0 +1,24 @@
+#IC 1st function notes
+#Set variable first then we define all of our functions
+
+def add(x, y):
+    return x+y
+
+
+def initials(name):
+    names = name.split(" ")
+    intitials = " "
+    for name in names:
+        initials+=name[0]
+
+    return initials
+
+total = add(5,5)
+print(total)
+print(f"10+72 = {add(10,72)}")
+x = 0
+while x < add(3,9):
+    print("duck")
+    x+= 1
+
+print("goose!")
