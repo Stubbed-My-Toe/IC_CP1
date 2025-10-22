1ee869cf7cb47dca6c2e89a1a24e33bda1baaaf3
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2025-10-22 15:13:01 +0000

    cesar

--- assignments/cesar_cypher.py
+++ assignments/cesar_cypher.py
@@ -1,18 +1,19 @@
 #IC 1st Cesar cypher
 
 #establish alphabet
-alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+alphabet = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 #Ask what operation user wants to do
-operation = input("What operation do you want to do? Press 1 for encode. Press 2 for decode.\n").strip
-true_operation = operation.isdigit()
-if operation == 1:
-    #get cipher
+operation =input("What operation do you want to do? Press 1 for encode. Press 2 for decode.\n").strip()
+
+if operation == "1":
     raw_cipher = input("Please pick a number for the cesar cypher\n").strip()
     cipher = raw_cipher.isdigit()
     if cipher:
-        print(f"Nice choice! Shifting cipher by {raw_cipher}.")
-        alphabet.rotate(raw_cipher)
-        print(alphabet)
+        print(f"Nice choice!")
+        message = input("Now, enter the message you wanted to encode!\n")
+        for i in message:
+           bleh= alphabet.find(i)
+           print(bleh)
     else:
         print("The number you entered is not valid. Exiting program now...")
         quit()
@@ -23,5 +24,5 @@ elif operation == 2:
     if cipher:
         print(f"Alright, shifting by {raw_cipher} now.")
 else:
-    print("The input is invalid exiting program now")
+    print("The input is invalid, exiting program now")
     quit()
--- assignments/tempCodeRunnerFile.py
+++ assignments/tempCodeRunnerFile.py
@@ -1,1 +1,8 @@
-input = print("Please pick a number for the cesar cypher")
+input("What operation do you want to do? Press 1 for encode. Press 2 for decode.\n").strip()
+if input == 1:
+    operation += 1
+elif input == 2:
+    operation += 2
+    print("op is 2")
+else:
+    print("please try again later")
