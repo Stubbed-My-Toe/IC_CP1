cb75bee202af057710001d386a1900e2fc229cb0
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2025-10-22 21:26:25 +0000

    cesar

--- assignments/cesar_cypher.py
+++ assignments/cesar_cypher.py
@@ -1,22 +1,23 @@
 #IC 1st Cesar cypher
-def shift():
-    for i in message:
-        pos = alphabet.index(i)
-    return pos + raw_cipher
+
+#pick a number for the cesar cypher
 #establish alphabet
 alphabet = ("abcdefghijklmnopqrstuvwxyz")
 #Ask what operation user wants to do
-operation =input("What operation do you want to do? Press 1 for encode. Press 2 for decode.\n").strip()
+operation =input("What operation do you want to do? Press 1 to encode. Press 2 to decode.\n").strip()
+raw_cipher = int(input("Please pick a number for the cesar cypher. A is at 0, B is at 1, C is at 2, and so on\n").strip())
+def shift():
+    if raw_cipher >= 26:
+        raw_cipher % 26
 #if statement for 1 and 2
 if operation == "1":
-    #pick a number for the cesar cypher
-    raw_cipher = input("Please pick a number for the cesar cypher\n").strip()
     #check if cypher is valid.
-    cipher = raw_cipher.isdigit()
+    cipher = raw_cipher
     if cipher:
         print(f"Nice choice!")
         message = input("Now, enter the message you wanted to encode!\n")
         shift()       
+        print()
     else:
         print("The number you entered is not valid. Exiting program now...")
         quit()
