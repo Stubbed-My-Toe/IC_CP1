8179f7f4a63c19fd4063ef5d07470ba94329bc1b
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2025-10-22 09:54:22 -0600

    cesar

--- assignments/cesar_cypher.py
+++ assignments/cesar_cypher.py
@@ -1,19 +1,22 @@
 #IC 1st Cesar cypher
-
+def shift():
+    for i in message:
+        pos = alphabet.index(i)
+    return pos + raw_cipher
 #establish alphabet
-alphabet = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+alphabet = ("abcdefghijklmnopqrstuvwxyz")
 #Ask what operation user wants to do
 operation =input("What operation do you want to do? Press 1 for encode. Press 2 for decode.\n").strip()
-
+#if statement for 1 and 2
 if operation == "1":
+    #pick a number for the cesar cypher
     raw_cipher = input("Please pick a number for the cesar cypher\n").strip()
+    #check if cypher is valid.
     cipher = raw_cipher.isdigit()
     if cipher:
         print(f"Nice choice!")
         message = input("Now, enter the message you wanted to encode!\n")
-        for i in message:
-           bleh= alphabet.find(i)
-           print(bleh)
+        shift()       
     else:
         print("The number you entered is not valid. Exiting program now...")
         quit()
--- assignments/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-input("What operation do you want to do? Press 1 for encode. Press 2 for decode.\n").strip()
-if input == 1:
-    operation += 1
-elif input == 2:
-    operation += 2
-    print("op is 2")
-else:
-    print("please try again later")
