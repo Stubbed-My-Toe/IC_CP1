d703251b5a882b0dd9b51f9d2362a5c568090352
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2025-10-23 09:53:25 -0600

    caesar

--- assignments/cesar_cypher.v2.py
+++ assignments/cesar_cypher.v2.py
@@ -1,21 +1,23 @@
 #IC 1st Caesar Cypher
-import string
-#estalish alphabet
-string.ascii_lowercase
-string.ascii_uppercase
 
 #ask for operation.
 operation = input("What operation do you want to do? Press 1 to encode. Press 2 to decode.\n").strip()
 
+#ask for text and key
+text = str(input("What is the text you want to use?\n")).strip().lower()
+key = int(input("What is the key to the cipher?\n"))
+#figure out end index
+
+def shift(selected_letters):
+    s = alphabet[0:end_index]
+    
+def cipher():
+#estalish alphabets
 if operation == "1":
-    #estalish alphabet
-    alphabet = ("abcdefghijklmnopqrstuvwxyz")
-    #Ask for cipher key and text
-    text = str(input("What is the message you want to encode?\n")).strip().lower()
-    key = int(input("What is the key to the cipher?\n"))
-    if 0 < key <= 26:
-        for i in text:
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
 elif operation == 2:
     back_alphabet = "zyxwvutsrqponmlkjihgfedcba"
 else:
-    print("You need to enter a number between 1 and 26.")
+    print("You need to enter either 1 or 2")
+selected_letters = alphabet[0:key]
+    
