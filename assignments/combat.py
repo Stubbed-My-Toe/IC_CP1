00ef47329ccdd95977396a36872e60ad80483cec
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2025-10-28 09:10:55 -0600

    combat

--- assignments/combat.py
+++ assignments/combat.py
@@ -6,25 +6,12 @@ name = input("What is your name?\n")
 grade = int(input("Press 1 if you are a fighter, 2 if you are a mage, and 3 if you are a rogue.\n"))
 diff = int(input("What is your desired difficulty of combat? Press 1 for easy, 2 for medium, and 3 for hard.\n"))
 
-
-if diff == 1:
-    
-elif diff == 2:
-    print()
-elif diff == 3:
-    print()
-else:
-    print("Enter a valid number!")  
-    diff = 0
-
-        
 def stats():
     if grade == 1:
         print("You are a fighter.\nThey specialize in combo building melee combat.\nFighters deal moderate damage, and have high health with moderate defense")
-    elif grade == 2:
-        print("You are a mage.\nThey specialize in longer range magic combat, and have good utility.\nThey deal moderate damage, have low health, and high defense.")
         
+    elif grade == 2:
+        print("You are a mage.\nThey specialize in longer range magic combat, and have good utility.\nThey have low health with moderate damage and high defense.")
     elif grade == 3:
-        print("You are a rogue.\nThey specialize in stealth and ambush tactics from close to medium range.\nThey deal high damage, have moderate health, and low defense")
-    else:
-        print("You need to enter a valid input to access stats!")
+        print("You are a rogue.\nThey specialize in stealth and ambush tactics from close to medium range.\nThey deal high damage, have moderate health, and a low defense")
+
