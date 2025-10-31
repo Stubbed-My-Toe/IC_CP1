4f130ab8e42f8d11499ac89307d787d90c5eec65
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2025-10-31 09:51:28 -0600

    turtmaze

---
+++ assignments/turtmaze.py
@@ -0,0 +1,16 @@
+#IC 1st Maze generator py
+
+#Import turtle as t
+#make screen and screen color
+#msakes maze turtle
+#maze width and height needs to be defined at least a six by six
+#Room for six paths to head straight down from top to bottom.
+#w == wall
+#P == path
+#[      ,
+#       ,
+#       ,
+#       ,]
+#inside of maze needs to be defined
+#Define start and end of maze
+#start making turtle make maze
