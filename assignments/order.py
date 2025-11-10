3644f098ad0720dda7193db8cdd6171d0f7d9d85
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2025-11-10 09:53:18 -0700

    order

--- assignments/order.py
+++ assignments/order.py
@@ -1,1 +1,4 @@
 #IC 1st Order Up
+
+#Create a program that allows a user to order items off a menu. Menu items need to be stored in a dictionary with their prices as the value. Users need to be able to order a drink, a main course and two side dishes. At the end the program needs to repeat back to the user their full order, and the total cost. 
+
