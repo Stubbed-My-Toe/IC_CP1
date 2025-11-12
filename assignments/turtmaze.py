11775cc6d9e0450a5a248cc9a1c17d252ba3cefe
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2025-11-12 09:52:54 -0700

    turtmaze

--- assignments/turtmaze.py
+++ assignments/turtmaze.py
@@ -17,42 +17,66 @@ turt.pencolor("white")
 turt.color("white")
 #Define turt start
 startpoint = turt.teleport(-300,-300)
-#define frame of maze which is 600x600 with 50px holes at 300x300 and -300x-300
-def frame():
-    turt.teleport(-300,-300)
-    turt.penup()
-    turt.forward(50)
-    turt.pendown()
-    turt.forward(550)
-    turt.left(90)
-    turt.forward(600)
-    turt.left(90)
-    turt.penup()
-    turt.forward(50)
-    turt.pendown()
-    turt.forward(550)
-    turt.left(90)
-    turt.forward(600)
+#define frame of maze which == 600x600 with 50px holes at 300x300 and -300x-300
+#def frame():
+    #turt.pendown()
+    #turt.forward(550)
+    #turt.left(90)
+    #turt.forward(600)
+    #turt.left(90)
+    #turt.penup()
+    #turt.forward(50)
+    #turt.pendown()
+    #turt.forward(550)
+    #turt.left(90)
+    #turt.forward(600)
 
 
-frame()
+#frame()
 #def grid of maze.grid
-ungrid =[" "," "," "," "," "," ",
-        " "," "," "," "," "," ",
-        " "," "," "," "," "," ",
-        " "," "," "," "," "," ",
-        " "," "," "," "," "," ",
-        " "," "," "," "," "," "]
+grid_rows=[["1", "", "", "", "", "", "", "1"], 
+           ["1", "", "", "", "", "", "", "1"], 
+           ["1", "", "", "", "", "", "", "1"], 
+           ["1", "", "", "", "", "", "", "1"], 
+           ["1", "", "", "", "", "", "", "1"], 
+           ["1", "", "", "", "", "", "", "1"]]
 
-def grid_y():
-    len(ungrid)
-    print(len(ungrid))
-    for x, y in ungrid:
-        turt.teleport(startpoint)
-        turt.forward(600)
-        y
-grid_y()
+grid_columns=[["0", "", "", "", "", "", "", "1"], 
+              ["1", "", "", "", "", "", "", "1"], 
+              ["1", "", "", "", "", "", "", "1"], 
+              ["1", "", "", "", "", "", "", "1"], 
+              ["1", "", "", "", "", "", "", "1"], 
+              ["1", "", "", "", "", "", "", "0"]]
 
+
+for x, row in enumerate(grid_rows):
+    for y, cell in enumerate(row):
+        if cell == "":
+            grid_rows[x][y] = random.choice(["0","1"])
+
+for y, column in enumerate(grid_columns):
+    for x, cell in enumerate(column):
+        if cell == "":
+            grid_columns[y][x] = random.choice(["0","1"])
+
+turt.penup()
+for x, row in enumerate(grid_rows):
+    for y, cell in enumerate(row):
+        if cell == "1":
+            turt.goto(x*20-200,y*20-200)
+            turt.setheading(90)
+            turt.pendown()
+            turt.forward(20)
+            turt.penup()
+for y, columns in enumerate(grid_columns):
+    for x, cell in enumerate(columns):
+        if cell == "1":
+            turt.goto(x*20-200,y*20-200)
+            turt.setheading(0)
+            turt.pendown()
+            turt.forward(20)
+            turt.penup()
+            
 
         
 screen.exitonclick()
@@ -69,8 +93,7 @@ screen.exitonclick()
 #       ,]
 #inside of maze needs to be defined
 #grid_rows=[]
-grid_rows=[]
-grid_columns=[]
-#grid_columns=[1,1,1,1,#you get the gist]
+
+#grid_columns=[1,1,1,1,#you get the g==t]
 #Define start and end of maze
 #start making turtle make maze
