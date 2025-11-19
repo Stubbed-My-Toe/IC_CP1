5c3dfeb8642852b2fd45af75f55d2c908ec28ffb
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2025-11-19 22:37:23 +0000

    calc

--- assignments/flexible_calc.py
+++ assignments/flexible_calc.py
@@ -1,20 +1,46 @@
 #IC 1st flexible calculator
 
+import math
+
 #Print(welcome message)
+print("Welcome to the flexible calculator!")
 
 #available operations= sum, which is x+b. average, which is x+b divided by how many characters. max, which is the highest number. min, which is the lowest. Product which is x*b.
+operation=input("What operation do you want to perform? Sum, Average, Max, Min, or Product?\n")
 #ask which one.\n
 
+
+
+#def sum(x,b)
+def sum(*nums):
+#nums = enter numbers
+nums=print("Enter numbers (type 'done' when finished):")
+
+#for num in nums
+    #1+2+3+4+5 etc
+    #=bleh
+
+#def average()
+#for num in nums, add number and divide by how many numbers there are
+    #(1+2+3)/3
+
+#def max()
+#what is the highest number in the list?
+#organize them by value and pick the index with the highest number
+
+#defmin()
+#same asmax, but the smallest number instead
+
+#defproduct()
+#product_answ = x*b*c
+
+
 #while loop
-# #nums = enter numbers
+
 #have them keep putting num in until
 #stop when input = done
 #call func and do calculation
 #ask do you want to go again?
 #if input = yes keep going
+#reset nums
 #else= print thank you for using the calcuator! break
-
-#def sum(x,b)
-#for num in nums
-    #
-
