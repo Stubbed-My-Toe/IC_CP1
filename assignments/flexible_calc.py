fbbc773e8a0de0a18afb89b12d56c592ef052fa0
StubbedMyToe <isaac.covington@ucas-edu.net>
2025-11-19 09:52:05 -0700

    flexible

---
+++ assignments/flexible_calc.py
@@ -0,0 +1,20 @@
+#IC 1st flexible calculator
+
+#Print(welcome message)
+
+#available operations= sum, which is x+b. average, which is x+b divided by how many characters. max, which is the highest number. min, which is the lowest. Product which is x*b.
+#ask which one.\n
+
+#while loop
+# #nums = enter numbers
+#have them keep putting num in until
+#stop when input = done
+#call func and do calculation
+#ask do you want to go again?
+#if input = yes keep going
+#else= print thank you for using the calcuator! break
+
+#def sum(x,b)
+#for num in nums
+    #
+
