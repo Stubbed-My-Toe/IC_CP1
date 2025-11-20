795b0f5cbb4a1b739d1732b953a2a02f9aa18a1b
StubbedMyToe <isaac.covington@ucas-edu.net>
2025-11-20 09:36:54 -0700

    squared

---
+++ assignments/squared_numbers.py
@@ -0,0 +1,7 @@
+#IC 1st squared numbers
+
+#list o num
+#define new num and its lambda
+import math ; number=[14,591,538,68,650,42,12,301,694,228,528,987,679,664,983,657,325,428,382];new_num=map(lambda IJM: IJM**2,number)
+# for loop, and print newcode as int and old num as int
+for num in new_num : print(f"New number:{int(num)} OG number {int(math.sqrt(num))}")
