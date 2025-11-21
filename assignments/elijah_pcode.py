6b2277bc648152410d1bf4cab71741341808de07
StubbedMyToe <isaac.covington@ucas-edu.net>
2025-11-21 09:52:10 -0700

    elijah

---
+++ assignments/elijah_pcode.py
@@ -0,0 +1,26 @@
+#IC 1st elijah code factorial
+
+#What is sys?
+#import it
+import sys
+
+
+#we need some way for the user to put in number
+input(int("What number do you want for the factorial?"))
+
+#use sys to set higher max terminal digits
+#i don't know how to do that^
+
+#define function Fac()#what?
+def fac():
+    #try and except value error:statement with user input for the number.  #how do i do this?
+
+    #while true:
+        #input*input -1
+        #if input is = to 1
+        #break
+
+    #if input is numeric:
+    if 
+        #fac()
+#print(f"{input} was the original input and {final_num})#what is the answer? And how is it defined? is there some way to store the original input?
