3e1e6f51e9157abb3c0679bd0a70a686f34393f9
StubbedMyToe <isaac.covington@ucas-edu.net>
2025-12-01 09:52:03 -0700

    cowboy

--- assignments/SP_cowboy.py
+++ assignments/SP_cowboy.py
@@ -1,1 +1,56 @@
 #IC 1st Final Project: SP Cowboy
+
+#will be a rogue-like where the player can pick up items and upgrades and eventually get to the boss to fight them.
+
+#import random
+#import time
+
+###########################################################################################
+
+#STATS FOR SURVIVAL
+#These make sure that the player does not die instantly, or so the player can get really strong. Are meant to be unbalanced.
+
+
+#HEALTH
+#Health is 70 at base for character creation, with exceptions for grenadier
+    #Determines if the player will die to certain shots, will be called grit in actual gameplay
+
+
+
+
+#DEFENSE
+#defense is 70 at base. It is the ability to tank shots without bleeding or taking any actual damage.
+    #Determines if the player will be able to block attacks without taking damage. Will be caled gumption in actual gameplay.
+
+
+
+#DODGE
+#Ducking behind barrels, moving quick and good a good eye can keep the player from getting hurt. Strts at 50, goes up with certain classes.
+    #things like dodging and ducking in rooms, moving fast and being shifty. Called speed in actual gameplay.
+
+
+
+#CARRY
+#determines how much the player can lift. Starts at 150 but can go up with upgrades and certain classes' abilities.
+    #Determines how much the player can lift. will sort of work like the weight function in fallout. Too much weight (over 60% of max) leads to a decrease in speed. Called mule in gameplay.
+
+
+
+#################################################################################################################################
+
+#3 CLASSES 
+#with different stats like dodge, health, defense and carry weight
+
+
+#guerilla
+
+
+#gunslinger
+
+
+#grenadier
+
+
+
+###########################################################################################################################
+
