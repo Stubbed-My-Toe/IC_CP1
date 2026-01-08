ee8a5863970e7d6a411de3fd6de8ce3dbefffe0a
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2026-01-08 09:51:53 -0700

    rpg and finance

--- assignments/Financial_calculator.py
+++ assignments/Financial_calculator.py
@@ -1,1 +1,18 @@
-#IC
+#IC 1st Finance Calculator
+
+#Saving calculator: 
+    #Deposit = Int(How much do you deposit to savings per month?)
+    #savings goal = int(What is your savings goal?)
+    #How long it takes to save = savings goal/deposit
+#return (f"{how long it takes to save} months
+
+#compound interest calculator
+    #time in years = How many years do you want to save?
+    #For years in time in years:
+        #
+
+#Welcome message: "What would you like to use today?"
+#Show Options: Savings Calculator, Compound Interest Calculator, Budget Allocator, Sale Price Allocator, Tip Calculator
+
+#If savings
+    #Savings 
