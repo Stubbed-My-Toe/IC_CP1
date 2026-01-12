f3c56729e0548bff657bee2ab6c64f1a85559da4
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2026-01-12 09:52:55 -0700

    finance calc

--- assignments/Financial_calculator.py
+++ assignments/Financial_calculator.py
@@ -3,7 +3,7 @@
 #Saving calculator: 
 def savings_calculator():
     #savings goal = int(What is your savings goal?)
-    savings_goal = int(input("what is your savings goal?\n"))
+    savings_goal = int(input("What is your savings goal?\n"))
     #how_often = How often do you want to deposit?
     how_often = int(input("How often do you want to deposit?\n1. Weekly\n2. Monthly\n"))
     def WorM():
@@ -12,7 +12,7 @@ def savings_calculator():
         elif how_often == 2:
             timeframe = "Months"
         else:
-            timeframe == "however often you are depositing"
+            timeframe == "However often you are depositing"
         return timeframe
     timeframe = WorM()
     #Deposit = Int(How much do you deposit to savings each time??)
@@ -22,20 +22,32 @@ def savings_calculator():
     final_statement = print(f"It will take {how_long} {timeframe} to save up")
     return final_statement
 #return (f"{how long it takes to save} months
-savings_calculator()
 
 
 #compound interest calculator
+def compound_interest():
     #Balance = What is your preexisting balance
+    balance = int(input("What is your pre-existing balance\n"))
     #time in years = How many years do you want to save?
+    time_in_years = int(input("How many years do you want to save\n"))
     #Interest rate = What is your interest rate?
+    interest_rate = float(input("What is the interest rate? (Enter with no percentage sign. ie: instead of 5%, use 5. and, use a point in front of the numbers, if interest is 50%, use 1.5)\n"))
     #For years in time in years:
-        #multiply balance by interest rate
+    answer=balance * interest_rate
+    print(f"You will have {answer}")
 #return
 
 #Budget allocator
+def budget_allocator():
     #What is the amount you want to use on the budget?
+    income = float(input("What is your income?\n"))
     #Divide balance by 100, and then multiply by 40 for rent. Multiply 15 for food and 5 for bills. 10 for savings and 30 for transportation
+    def split_cash():
+        income/=100
+        rent = income*40
+        food = income*15
+        utility = income*5
+        savings = income*10
 #return
 
 #Sales price calculator:
