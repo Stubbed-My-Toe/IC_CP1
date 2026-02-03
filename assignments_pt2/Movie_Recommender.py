196af49e541c178440a3a6f3cee4a7aea6367384
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2026-02-03 09:52:41 -0700

    Movies

---
+++ assignments_pt2/Movie_Recommender.py
@@ -0,0 +1,6 @@
+#IC 1st period CP2 movie recommender
+
+import csv
+#welcome to the program. This is so you can pick movies. perchance.
+#def func main_menu()
+    #What do you want to search for? Your father? Womp-Womp.
